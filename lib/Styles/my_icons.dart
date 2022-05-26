899a5ace62d72309b69e3dc45f04367cc2e7409b
SomabhaiMistry <vaibhav.mis96@gmail.com>
2022-05-26 16:55:01 +0530

    PastFeature 1 Screen Design by Vaibhav

--- lib/Styles/my_icons.dart
+++ lib/Styles/my_icons.dart
@@ -1,5 +1,6 @@
 
 const img_girl = 'assets/images/img_girl.png';
+const img_giral_crop = 'assets/images/img_giral_crop.png';
 const img_close = 'assets/images/close.png';
 const img_more = 'assets/images/more.png';
 const setting_girl_img = 'assets/images/setting_girl_img.png';
@@ -71,3 +72,5 @@ const icon_cancel = 'assets/images/icon_cancel.svg';
 const icon_feature_next_arrow = 'assets/images/icon_feature_next_arrow.svg';
 const icon_filter_orange = 'assets/images/icon_filter_orange.svg';
 const icon_play_list_orange = 'assets/images/icon_play_list_orange.svg';
+const icon_light_heart = 'assets/images/icon_light_heart.svg';
+const icon_share = 'assets/images/icon_share.svg';
