aa94d56121a649129c8aedba2e8bce8f110dbad6
SomabhaiMistry <vaibhav.mis96@gmail.com>
2022-05-27 13:32:01 +0530

    PastFeature 1 Screen Design Update by Vaibhav

--- lib/UIScreen/Test.dart
+++ lib/UIScreen/Test.dart
@@ -35,72 +35,75 @@ class _TestState extends State<Test> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Container(
+              //   margin: EdgeInsets.only(left: 24.w,right: 24.w,top: 16.w),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.start,
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       Expanded(
+              //         child: InkWell(
+              //           onTap: () {
+              //             setState(() {
+              //               Bg1 = true;
+              //               Bg2 = false;
+              //               tv1 = true;
+              //               tv2 = false;
+              //             });
+              //           },
+              //           child: Container(
+              //             padding: EdgeInsets.symmetric(vertical: 15.h),
+              //             decoration: BoxDecoration(
+              //               color:Bg1?black_121212: grey_f5f5f5,
+              //               borderRadius: BorderRadius.circular(4.r),
+              //             ),
+              //             child: Center(
+              //               child: Text("Transactions",style: TextStyle(
+              //                   fontSize: 16.sp,color: tv1?Colors.white: black_121212,
+              //                   fontFamily: roboto_bold
+              //               ),),
+              //             ),
+              //           ),
+              //         ),
+              //       ),
+              //
+              //       SizedBox(width: 16.w,),
+              //
+              //       Expanded(
+              //         child: InkWell(
+              //           onTap: () {
+              //             setState(() {
+              //               Bg1 = false;
+              //               Bg2 = true;
+              //               tv1 = false;
+              //               tv2 = true;
+              //             });
+              //           },
+              //           child: Container(
+              //             padding: EdgeInsets.symmetric(vertical: 15.h),
+              //             decoration: BoxDecoration(
+              //               color:Bg2?black_121212: grey_f5f5f5,
+              //               borderRadius: BorderRadius.circular(4.r),
+              //             ),
+              //             child: Center(
+              //               child: Text("Payouts",style: TextStyle(
+              //                   fontSize: 16.sp,color:tv2 ?Colors.white: black_121212,
+              //                   fontFamily: roboto_bold
+              //               ),),
+              //             ),
+              //           ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+
               Container(
-                margin: EdgeInsets.only(left: 24.w,right: 24.w,top: 16.w),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: InkWell(
-                        onTap: () {
-                          setState(() {
-                            Bg1 = true;
-                            Bg2 = false;
-                            tv1 = true;
-                            tv2 = false;
-                          });
-                        },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(vertical: 15.h),
-                          decoration: BoxDecoration(
-                            color:Bg1?black_121212: grey_f5f5f5,
-                            borderRadius: BorderRadius.circular(4.r),
-                          ),
-                          child: Center(
-                            child: Text("Transactions",style: TextStyle(
-                                fontSize: 16.sp,color: tv1?Colors.white: black_121212,
-                                fontFamily: roboto_bold
-                            ),),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(width: 16.w,),
-
-                    Expanded(
-                      child: InkWell(
-                        onTap: () {
-                          setState(() {
-                            Bg1 = false;
-                            Bg2 = true;
-                            tv1 = false;
-                            tv2 = true;
-                          });
-                        },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(vertical: 15.h),
-                          decoration: BoxDecoration(
-                            color:Bg2?black_121212: grey_f5f5f5,
-                            borderRadius: BorderRadius.circular(4.r),
-                          ),
-                          child: Center(
-                            child: Text("Payouts",style: TextStyle(
-                                fontSize: 16.sp,color:tv2 ?Colors.white: black_121212,
-                                fontFamily: roboto_bold
-                            ),),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                height: 200,
+                child: ImageFiltered(
+                    imageFilter: ImageFilter.blur(sigmaY:10,sigmaX:10), //SigmaX and Y are just for X and Y directions
+                    child: Image.asset(img_girl,width: double.infinity,height: 200.h,fit: BoxFit.fill,) //here you can use any widget you'd like to blur .
                 ),
-              ),
-
-              ImageFiltered(
-                  imageFilter: ImageFilter.blur(sigmaY:10,sigmaX:10), //SigmaX and Y are just for X and Y directions
-                  child: Image.asset(img_girl,width: double.infinity,height: 200.h,fit: BoxFit.fill,) //here you can use any widget you'd like to blur .
               ),
             ],
           ),
